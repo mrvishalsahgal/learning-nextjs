83975e449eabfbd58ac9ba8192dc4a4d21c646a6
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-05-17 19:10:26 +0530

    learn : Private Folder

--- app/(application)/page.js
+++ app/(application)/page.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Component from "../_components/page";
 
 export default function Home() {
   return (
     <>
       <h1> Welcome to Next JS </h1>
+      <Component />
       <Link href="/about"> About </Link>
       <Link href="/contact"> Contact </Link>
       <Link href="/services"> Services </Link>
