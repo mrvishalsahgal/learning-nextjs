b78d2c08501e905f00da104868b32a80a3608d1f
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-05-17 18:23:12 +0530

    Learn : Meta Data Api

--- app/blogs/[blogID]/page.js
+++ app/blogs/[blogID]/page.js
@@ -1,3 +1,10 @@
+export async function generateMetadata({ params }) {
+  const { blogID } = await params;
+
+  return {
+    title: `Blog ${blogID}`,
+  };
+}
 export default async function DynamicBlog({ params }) {
   const { blogID } = await params;
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
-
+export const metadata = {
+  title:{
+    template:"%s | Learning NextJS",
+    default:"Learning NextJS"
+  },
+  description: "Learning NextJS",
+}
 
 export default function RootLayout({children}) {
   return (
